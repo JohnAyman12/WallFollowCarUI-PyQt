e517dcac6734c66f8af0b5af2da9aa2d0dc3429f
John Ayman <139935256+JohnAyman12@users.noreply.github.com>
2023-09-04 02:18:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from PyQt5.QtMultimediaWidgets import *
 from PyQt5.QtCore import *
 
 from MainPage import Ui_MainWindow as mainPage
-from Task1Page import Ui_MainWindow as task1
+from Task1Page import Ui_MainWindow as Task1
 from cameraFeed import Worker1 as cameraFeed
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,10 +16,28 @@ class MainWindow(QMainWindow):
         self.main_window = mainPage()
         self.main_window.setupUi(self)
 
+        # modifying main page appearance
+        self.setStyleSheet("background-color: #001522;")
+        self.main_window.sensorReadingBox.setStyleSheet('color: white')
+        self.main_window.ipTasksBox.setStyleSheet('color: white')
+        self.main_window.cameraFeedBox.setStyleSheet('color: white')
+        self.main_window.teamLogoBox.setStyleSheet('color: white')
+        self.main_window.motionControlBox.setStyleSheet('color: white')
+        self.main_window.speedControlBox.setStyleSheet('color: white')
+        self.main_window.motionModeBox.setStyleSheet('color: white')
+        self.main_window.connectivityBox.setStyleSheet('color: white')
+
         # initializing IP task 1 window
         self.window2 = QMainWindow()
-        self.task1_window = task1()
+        self.task1_window = Task1()
         self.task1_window.setupUi(self.window2)
+
+        # modifying task-1 page appearance
+        self.window2.setStyleSheet("background-color: #001522;")
+        self.task1_window.input1Lbl.setStyleSheet('color: white')
+        self.task1_window.input2Lbl.setStyleSheet('color: white')
+        self.task1_window.outputLbl.setStyleSheet('color: white')
+        self.task1_window.exitButton.setStyleSheet('color: white')
 
         # setting main page camera feed
         self.camera = cameraFeed()
@@ -27,14 +46,8 @@ class MainWindow(QMainWindow):
         self.main_window.screenShotBtn.clicked.connect(self.camera.savePhoto)
 
         # setting team logo part in main window
-        pixmap = QPixmap("images\\teamLogo.png")
+        pixmap = QPixmap("images\\teamLogo3.png")
         self.main_window.logoLbl.setPixmap(pixmap)
-
-        # setting motion control buttons slots
-        self.main_window.forwardBtn.pressed.connect(self.move_forward)
-        self.main_window.backwardBtn.pressed.connect(self.move_backward)
-        self.main_window.rightBtn.pressed.connect(self.move_right)
-        self.main_window.leftBtn.pressed.connect(self.move_left)
 
         # setting speed control part
         self.main_window.lowSpeedRadioBtn.clicked.connect(self.low_speed)
@@ -42,35 +55,91 @@ class MainWindow(QMainWindow):
         self.main_window.highSpeedRadioBtn.clicked.connect(self.high_speed)
         self.main_window.speedSpinBox.valueChanged.connect(self.speed_value_changed)
 
+        # setting control buttons
+        self.main_window.forwardBtn.pressed.connect(self.forward_btn_clicked)
+        self.main_window.backwardBtn.pressed.connect(self.backward_btn_clicked)
+        self.main_window.rightBtn.pressed.connect(self.right_btn_clicked)
+        self.main_window.leftBtn.pressed.connect(self.left_btn_clicked)
+
         # setting video players for IP task 1
         self.vid_widget1 = QVideoWidget()
         self.task1_window.vid1VLayout.addWidget(self.vid_widget1)
         self.media_player1 = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.media_player1.setVideoOutput(self.vid_widget1)
-        self.media_player1.setMedia(QMediaContent(QUrl.fromLocalFile("images\\IPvid.mp4")))
+        self.media_player1.setMedia(QMediaContent(QUrl.fromLocalFile("images\\IP_vid.mp4")))
         self.media_player1.stateChanged.connect(self.mediastate1_changed)
 
         self.vid_widget2 = QVideoWidget()
         self.task1_window.vid2VLayout.addWidget(self.vid_widget2)
         self.media_player2 = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.media_player2.setVideoOutput(self.vid_widget2)
-        self.media_player2.setMedia(QMediaContent(QUrl.fromLocalFile("images\\IPvid.mp4")))
+        self.media_player2.setMedia(QMediaContent(QUrl.fromLocalFile("images\\IP_vid.mp4")))
         self.media_player1.stateChanged.connect(self.mediastate2_changed)
 
         self.vid_widget3 = QVideoWidget()
         self.task1_window.outputVidHLayout.addWidget(self.vid_widget3)
         self.media_player3 = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.media_player3.setVideoOutput(self.vid_widget3)
-        self.media_player3.setMedia(QMediaContent(QUrl.fromLocalFile("images\\IPvid.mp4")))
+        self.media_player3.setMedia(QMediaContent(QUrl.fromLocalFile("images\\IP_vid.mp4")))
         self.media_player1.stateChanged.connect(self.mediastate3_changed)
 
         self.main_window.task1Btn.clicked.connect(self.open_task1_page)  # opening task-1 page event
         self.task1_window.exitButton.clicked.connect(self.close_task1_page)  # closing task-1 page event
 
-        self.task1_page_opened = False # to detect if task 1 page opened or not
+        self.task1_page_opened = False  # to detect if task 1 page opened or not
 
-    def image_update_slot(self, Image):  # shows camera feed (always running)
-        self.main_window.camerFeed.setPixmap(QPixmap.fromImage(Image))
+    def image_update_slot(self, image):  # shows camera feed (always running)
+        self.main_window.camerFeed.setPixmap(QPixmap.fromImage(image))
+
+    def low_speed(self):  # sets the speed mode to low
+        self.main_window.speedSpinBox.setValue(85)
+
+    def mid_speed(self):  # sets the speed mode to medium
+        self.main_window.speedSpinBox.setValue(170)
+
+    def high_speed(self):  # sets the speed mode to high
+        self.main_window.speedSpinBox.setValue(255)
+
+    def speed_value_changed(self):  # tells which speed mode we are in, depending on the value in the spin box
+        speed = self.main_window.speedSpinBox.value()
+        print(speed)
+        if speed <= 85:
+            print("Low")
+            self.main_window.lowSpeedRadioBtn.setChecked(True)
+        elif speed <= 170:
+            print("Mid")
+            self.main_window.midSpeedRadioBtn.setChecked(True)
+        else:
+            print("High")
+            self.main_window.highSpeedRadioBtn.setChecked(True)
+
+    def keyPressEvent(self, event):
+        if event.key() > 1000:
+            print("Make keyboard english, please!")
+        if event.key() == 87:
+            print("Forward")
+            self.main_window.forwardBtn.animateClick()
+        elif event.key() == 83:
+            print("Backward")
+            self.main_window.backwardBtn.animateClick()
+        elif event.key() == 68:
+            print("Right")
+            self.main_window.rightBtn.animateClick()
+        elif event.key() == 65:
+            print("Left")
+            self.main_window.leftBtn.animateClick()
+
+    def forward_btn_clicked(self):
+        print("Forward")
+
+    def backward_btn_clicked(self):
+        print("Backward")
+
+    def right_btn_clicked(self):
+        print("Right")
+
+    def left_btn_clicked(self):
+        print("Left")
 
     def open_task1_page(self):  # opens task-1 page when called
         self.task1_page_opened = True
@@ -98,39 +167,6 @@ class MainWindow(QMainWindow):
         if self.media_player3.state() == QMediaPlayer.StoppedState and self.task1_page_opened:
             self.media_player3.play()
 
-    def move_forward(self):  # sends a signal to move the car forward via the communication protocol
-        print("Forward")
-
-    def move_backward(self):  # sends a signal to move the car backward via the communication protocol
-        print("Backward")
-
-    def move_right(self):  # sends a signal to turn the car right via the communication protocol
-        print("Right")
-
-    def move_left(self):  # sends a signal to turn the car left via the communication protocol
-        print("Left")
-
-    def low_speed(self):  # sets the speed mode to low
-        self.main_window.speedSpinBox.setValue(85)
-
-    def mid_speed(self):  # sets the speed mode to medium
-        self.main_window.speedSpinBox.setValue(170)
-
-    def high_speed(self):  # sets the speed mode to high
-        self.main_window.speedSpinBox.setValue(255)
-
-    def speed_value_changed(self):  # tells which speed mode we are in, depending on the value in the spin box
-        speed = self.main_window.speedSpinBox.value()
-        print(speed)
-        if speed <= 85:
-            print("Low")
-            self.main_window.lowSpeedRadioBtn.setChecked(True)
-        elif speed <= 170:
-            print("Mid")
-            self.main_window.midSpeedRadioBtn.setChecked(True)
-        else:
-            print("High")
-            self.main_window.highSpeedRadioBtn.setChecked(True)
 
 if __name__ == '__main__':
     app = QApplication([])
